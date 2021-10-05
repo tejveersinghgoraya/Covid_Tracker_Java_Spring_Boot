2d1c1a0b9f5bd34e30b63a6ea748d150949cd059
tejveersinghgoraya <56366718+tejveersinghgoraya@users.noreply.github.com>
2021-10-05 11:00:38 +0100

    Spring_Initializer

---
+++ Covid-Tracker/src/test/java/io/tsg/CovidTracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tsg.CovidTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
